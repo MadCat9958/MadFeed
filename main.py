bdb54e76c53804a18e41cc6954a5bc3a820b6595
MadCat9958 <madcat9958@gmail.com>
2024-09-01 15:00:00 +0300

    l18n

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import asyncio
 import logging
 import sqlite3
 import traceback
+import json
 
 from dotenv import load_dotenv
 from aiogram import filters, F
@@ -17,6 +18,11 @@ dp = aiogram.Dispatcher()
 db = sqlite3.connect("messages.db", autocommit=True)
 db.row_factory = sqlite3.Row
 cur = db.cursor()
+
+locales = {}
+for locale in os.scandir("./locales"):
+    if not locale.name.endswith(".json") or not locale.is_file(): continue
+    locales[locale.name[:-5]] = json.load(open(locale.path))
 
 cur.execute(
     """CREATE TABLE IF NOT EXISTS messages (
@@ -31,16 +37,12 @@ cur.execute(
 
 @dp.message(filters.CommandStart())
 async def command_start_handler(message: types.Message):
-    await message.reply(
-        "Привет! На данный момент, бот работает в ограниченном режиме. Однако скоро ты сможешь удобно предлагать посты в @MadCat9958 любого типа. "
-        "Для удобства, будет возможность отдельно написать свой комментарий к посту, который, в зависимости от содержания, будет либо отправлен вместе с постом, либо нет.\n\n"
-        "А пока что, задонать плиз: @MadDonate_bot :)"
-    )
+    await message.reply(locales.get(message.from_user.language_code, locales["default"])["start_command_message"])
 
 @dp.message(filters.Command("cleardb"), F.from_user.id == int(os.getenv("BOT_OWNER_ID")))
 async def clear_db(message: types.Message):
     cur.execute("DELETE FROM messages")
-    await message.reply("База данных почищена!")
+    await message.reply(locales.get(message.from_user.language_code, locales["default"])["success"]["database_cleared"])
 
 @dp.message(F.from_user.id != int(os.getenv("BOT_OWNER_ID")), F.from_user.id == F.chat.id)
 async def resend_message(message: types.Message):
@@ -50,24 +52,24 @@ async def resend_message(message: types.Message):
         """INSERT INTO messages (msg_id, sender_id, original_message_id) VALUES (?, ?, ?)""",
         (msg.message_id, message.from_user.id, message.message_id)
     )
-    await msg.reply(f"Отправитель: {message.from_user.full_name} (ID: {message.from_user.id})")
-    await message.reply("Переслано!")
+    await msg.reply(f"Отправитель: {message.from_user.full_name} (ID: {message.from_user.id})") # hardcoded (feature)
+    await message.reply(locales.get(message.from_user.language_code, locales["default"])["success"]["sent_to_moderation"])
 
 @dp.message(F.from_user.id == int(os.getenv("BOT_OWNER_ID")), F.from_user.id == F.chat.id)
 async def answer_message(message: types.Message):
     if message.reply_to_message is None:
-        return await message.reply("Невозможно ответить на данное сообщение: это не предложка.")
+        return await message.reply(locales.get(message.from_user.language_code, locales["default"])["errors"]["invalid_message"])
     cur.execute("SELECT * FROM messages WHERE msg_id = ?", (message.reply_to_message.message_id,))
     msg = cur.fetchone()
     if msg is None:
-        return await message.reply("Невозможно ответить на данное сообщение: нет информации об предложившем пользователе.")
+        return await message.reply(locales.get(message.from_user.language_code, locales["default"])["errors"]["suggestor_not_found"])
     try:
         await message.copy_to(msg["sender_id"])
     except:
-        await message.reply("Ответ не был отправлен! Возможно, пользователь заблокировал бота либо он больше не существует.")
+        await message.reply(locales.get(message.from_user.language_code, locales["default"])["errors"]["reply_failed"])
         traceback.print_exc()
     else:
-        await message.reply("Ответ отправлен!")
+        await message.reply(locales.get(message.from_user.language_code, locales["default"])["success"]["answer_to_suggestor_sent"])
 
 async def main():
     await dp.start_polling(bot)
