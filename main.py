5d0e0ef51777eff32804de67cef3a780570fac1d
MadCat9958 <madcat9958@gmail.com>
2025-02-04 13:43:13 +0300

    feat: correct media group forwarding

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-import aiogram
-import os
-import sys
 import asyncio
-import logging
-import sqlite3
-import traceback
 import json
+import logging
+import os
+import sqlite3
+import sys
+import traceback
 
+import aiogram
+from aiogram import F, exceptions, filters, types
 from dotenv import load_dotenv
-from aiogram import filters, F
-from aiogram import types
 
 load_dotenv()
 
@@ -18,10 +17,12 @@ dp = aiogram.Dispatcher()
 db = sqlite3.connect("messages.db", autocommit=True)
 db.row_factory = sqlite3.Row
 cur = db.cursor()
+media_groups: dict[list[types.Message]] = {}
+MEDIA_GROUPS_FETCH_COOLDOWN = 0.6
 
 locales = {}
 for locale in os.scandir("./locales"):
-    if not locale.name.endswith(".json") or not locale.is_file(): 
+    if not locale.name.endswith(".json") or not locale.is_file():
         continue
     locales[locale.name[:-5]] = json.load(open(locale.path))
 
@@ -32,48 +33,176 @@ cur.execute(
         original_message_id BIGINT NOT NULL
     );"""
 )
+cur.execute(  # i created it for future button interactions (yeah, this note is for dementia)
+    """CREATE TABLE IF NOT EXISTS media_groups (
+        media_group_id BIGINT NOT NULL,
+        msg_id BIGINT NOT NULL
+    );"""
+)
+cur.execute(
+    """CREATE TABLE IF NOT EXISTS users (
+        user_id BIGINT NOT NULL,
+        is_banned BOOLEAN NOT NULL DEFAULT(false)
+    );"""
+)
 # msg_id - Message ID for getting message instance
 # sender_id - User ID, who sent the message
-# original_message_id - Original message ID in suggester's dialog 
+# original_message_id - Original message ID in suggester's dialog
+
 
 @dp.message(filters.CommandStart())
 async def command_start_handler(message: types.Message):
-    await message.reply(locales.get(message.from_user.language_code, locales["default"])["start_command_message"])
+    await message.reply(
+        locales.get(message.from_user.language_code, locales["default"])[
+            "start_command_message"
+        ]
+    )
 
-@dp.message(filters.Command("cleardb"), F.from_user.id == int(os.getenv("BOT_OWNER_ID")))
+
+@dp.message(
+    filters.Command("cleardb"), F.from_user.id == int(os.getenv("BOT_OWNER_ID"))
+)
 async def clear_db(message: types.Message):
     cur.execute("DELETE FROM messages")
-    await message.reply(locales.get(message.from_user.language_code, locales["default"])["success"]["database_cleared"])
+    await message.reply(
+        locales.get(message.from_user.language_code, locales["default"])["success"][
+            "database_cleared"
+        ]
+    )
 
-@dp.message(F.from_user.id != int(os.getenv("BOT_OWNER_ID")), F.from_user.id == F.chat.id)
+
+@dp.message(
+    F.from_user.id != int(os.getenv("BOT_OWNER_ID")), F.from_user.id == F.chat.id
+)
 async def resend_message(message: types.Message):
     owner_id = int(os.getenv("BOT_OWNER_ID"))
-    msg = await message.forward(owner_id,)
-    cur.execute(
-        """INSERT INTO messages (msg_id, sender_id, original_message_id) VALUES (?, ?, ?)""",
-        (msg.message_id, message.from_user.id, message.message_id)
+    if message.media_group_id is None:
+        msg = await message.forward(owner_id)
+        cur.execute(
+            """INSERT INTO messages (msg_id, sender_id, original_message_id) VALUES (?, ?, ?)""",
+            (msg.message_id, message.from_user.id, message.message_id),
+        )
+        await msg.reply(
+            f"Отправитель: {message.from_user.full_name} (ID: {message.from_user.id})"
+        )  # hardcoded (feature)
+        await message.reply(
+            locales.get(message.from_user.language_code, locales["default"])["success"][
+                "sent_to_moderation"
+            ]
+        )
+        return
+    if media_groups.get(message.media_group_id, None) is not None:
+        media_groups[message.media_group_id].append(message)
+        return
+    media_groups[message.media_group_id] = [message]
+    await asyncio.sleep(MEDIA_GROUPS_FETCH_COOLDOWN)
+    origs_ids = [m.message_id for m in media_groups[message.media_group_id]]
+    msgs = await bot.forward_messages(owner_id, message.chat.id, origs_ids)
+    for count, msg in enumerate(msgs):
+        cur.execute(
+            """INSERT INTO messages (msg_id, sender_id, original_message_id) VALUES (?, ?, ?)""",
+            (msg.message_id, message.from_user.id, origs_ids[count]),
+        )
+    await bot.send_message(
+        owner_id,
+        f"Отправитель: {message.from_user.full_name} (ID: {message.from_user.id})",
+        reply_parameters=types.ReplyParameters(message_id=msgs[0].message_id),
+    )  # hardcoded (feature)
+    del media_groups[message.media_group_id]
+    await message.reply(
+        locales.get(message.from_user.language_code, locales["default"])["success"][
+            "sent_to_moderation"
+        ]
     )
-    await msg.reply(f"Отправитель: {message.from_user.full_name} (ID: {message.from_user.id})") # hardcoded (feature)
-    await message.reply(locales.get(message.from_user.language_code, locales["default"])["success"]["sent_to_moderation"])
 
-@dp.message(F.from_user.id == int(os.getenv("BOT_OWNER_ID")), F.from_user.id == F.chat.id)
+
+@dp.message(
+    F.from_user.id == int(os.getenv("BOT_OWNER_ID")), F.from_user.id == F.chat.id
+)
 async def answer_message(message: types.Message):
+    logging.debug(message)
+    logging.debug(message.reply_to_message)
     if message.reply_to_message is None:
-        return await message.reply(locales.get(message.from_user.language_code, locales["default"])["errors"]["invalid_message"])
-    cur.execute("SELECT * FROM messages WHERE msg_id = ?", (message.reply_to_message.message_id,))
+        return await message.reply(
+            locales.get(message.from_user.language_code, locales["default"])["errors"][
+                "invalid_message"
+            ]
+        )
+    cur.execute(
+        "SELECT * FROM messages WHERE msg_id = ?",
+        (message.reply_to_message.message_id,),
+    )
     msg = cur.fetchone()
     if msg is None:
-        return await message.reply(locales.get(message.from_user.language_code, locales["default"])["errors"]["suggestor_not_found"])
+        return await message.reply(
+            locales.get(message.from_user.language_code, locales["default"])["errors"][
+                "suggestor_not_found"
+            ]
+        )
+    reply_to = types.ReplyParameters(message_id=msg["original_message_id"])
+    if message.media_group_id is None:
+        try:
+            await message.copy_to(msg["sender_id"], reply_parameters=reply_to)
+        except Exception as e:
+            if (
+                isinstance(e, exceptions.TelegramBadRequest)
+                and "message to be replied not found" in e.message
+            ):
+                try:
+                    await message.copy_to(msg["sender_id"])
+                except Exception as e:
+                    await message.reply(
+                        locales.get(
+                            message.from_user.language_code, locales["default"]
+                        )["errors"]["reply_failed"]
+                    )
+                    traceback.print_exc()
+                else:
+                    await message.reply(
+                        locales.get(
+                            message.from_user.language_code, locales["default"]
+                        )["success"]["answer_to_suggestor_sent"]
+                    )
+                return
+            await message.reply(
+                locales.get(message.from_user.language_code, locales["default"])[
+                    "errors"
+                ]["reply_failed"]
+            )
+            traceback.print_exc()
+        else:
+            await message.reply(
+                locales.get(message.from_user.language_code, locales["default"])[
+                    "success"
+                ]["answer_to_suggestor_sent"]
+            )
+        return
+    if media_groups.get(message.media_group_id, None) is not None:
+        media_groups[message.media_group_id].append(message)
+        return
+    media_groups[message.media_group_id] = [message]
+    await asyncio.sleep(MEDIA_GROUPS_FETCH_COOLDOWN)
+    origs_ids = [m.message_id for m in media_groups[message.media_group_id]]
     try:
-        await message.copy_to(msg["sender_id"])
+        await bot.copy_messages(msg["sender_id"], message.chat.id, origs_ids)
     except Exception:
-        await message.reply(locales.get(message.from_user.language_code, locales["default"])["errors"]["reply_failed"])
+        await message.reply(
+            locales.get(message.from_user.language_code, locales["default"])["errors"][
+                "reply_failed"
+            ]
+        )
         traceback.print_exc()
     else:
-        await message.reply(locales.get(message.from_user.language_code, locales["default"])["success"]["answer_to_suggestor_sent"])
+        await message.reply(
+            locales.get(message.from_user.language_code, locales["default"])["success"][
+                "answer_to_suggestor_sent"
+            ]
+        )
+
 
 async def main():
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
